958c3567264cb26fa555b351c36a407d48ad7b06
Drake Sorkhav <35drake@gmail.com>
2022-09-14 18:20:57 -0700

    I created the list used for tone interpretation, but haven't implemented it fully yet.

--- chinese.py
+++ chinese.py
@@ -1,3 +1,11 @@
+# This reference list interprets pinyin tones
+tones = ['ā',"a1",'á',"a2",'ǎ',"a3",'à',"a4",
+'ē',"e1",'é',"e2",'ě',"e3",'è',"e4",
+'ī',"i1",'í',"i2",'ǐ',"i3",'ì',"i4",
+'ō',"o1",'ó',"o2",'ǒ',"o3",'ò',"o4",
+'ū',"u1",'ú',"u2",'ǔ',"u3",'ù',"u4",
+'ǖ',"v1",'ǘ',"v2",'ǚ',"v3",'ǜ',"v4"]
+
 # Display Chinese text in the Windows terminal:
 from subprocess import check_output
 check_output("chcp 936", shell=True)
@@ -23,9 +31,17 @@ for row in range(len(lines)):
 		print(FC[row][col])
 
 
+
+
+# Drake, now you have to tl interpret the pinyin tones
+
 # Tasks remaining: display and test user on cards randomly (choose the col shown at random too)
-# Also, display the chinese toned vowels correctly somehow (ie display data 'ma4' as 'mà')
-# Note: āáǎà
+
+
+
+
+
+
 
 
 
