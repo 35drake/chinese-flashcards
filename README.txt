1ffcab2047781aab4bf590e98a6b0ac8ee595e75
Drake Sorkhav <35drake@gmail.com>
2022-09-21 17:24:27 -0700

    Added support for favorite/starred cards.

--- README.txt
+++ README.txt
@@ -1,1 +1,10 @@
 Hello, this is the readme file.
+
+Program by Drake Sorkhab, Sep 2022.
+
+Goals for future features:
+-Each time the program is run, it PERMANENTLY corrects the tone symbols by overwriting the "data.txt" file
+-Tracking which flashcards were right/wrong last time? So that you can choose to review only those
+-Separating radicals and words?
+-A huge bank of words scrubbed from a website put in (the program doesn't have to do this automatically, I could do it manually)
+-When the special only flashcards are displayed (input like "s5"), the number before each one should be its regular number (i.e. it won't necessarily go 0,1,2 but could be 34,51,56)
--- chinese.py
+++ chinese.py
@@ -3,10 +3,10 @@
 # This library used to shuffle lists
 import random
 
-# This debug function displays all flashcards; you must type literally "show_all(lines)"
-def show_all(lines):
+# This debug function displays all flashcards (FC var doesn't need to be passed)
+def show_all():
 	print('\n')
-	for row in range(len(lines)):
+	for row in range(len(FC)):
 		print(row,FC[row][0],FC[row][1],FC[row][2])
 
 # This module allows Windows to pause for a key press that doesn't have to be ENTER
@@ -15,7 +15,6 @@ def pause():
 	m.getch()
 	print()
 	
-
 # This reference list (48) interprets pinyin tones
 tones = ['ā',"a1",'á',"a2",'ǎ',"a3",'à',"a4",
 'ē',"e1",'é',"e2",'ě',"e3",'è',"e4",
@@ -32,38 +31,56 @@ check_output("chcp 936", shell=True)
 with open('data.txt',encoding="utf8") as f:
     lines = f.readlines()
 
-
-# Initialize the 'flashcards' matrix (character, pinyin, meaning):
-FC = []
-
-# Fill the 'flashcards' matrix
-for line in lines:
-	FC = FC + [line.split(",")]
-
+# Initialize and fill the 'flashcards' matrix
 # FC's row represents the item (quantity: len(lines))
 # FC's col represents the type (character, pinyin, or meaning)
+FC = []
+for line in lines:
+	FC = FC + [line.split(",")]
+	# The "lines" list shouldn't be used anymore, use "FC"
 
-# Correct the pinyin tones in each flashcard
-for row in range(len(lines)):
+# Fix the tones of each flashcard and mark them as special if they have '*'
+specials=[]
+for row in range(len(FC)):
 	for count in range(24):
+		
+		# Correct pinyin tones
 		FC[row][1]=FC[row][1].replace(tones[2*count+1],tones[2*count])
 
+		# Omit asterisk and add card to special list
+		if FC[row][2][-2] == '*':
+			FC[row][2] = FC[row][2][:-2] + FC[row][2][-1] #spot [-1] is just a '\n'
+			specials = specials+[row]
+
 # Give user menu
-choice = eval(input("\n\nPick which mode:\n\n1:Show character first\n2:Show pinyin first\n3:Show either first\n4:Display meaning first\n5:Display all cards\n\nChoice: "))
+choice = input("\n\nPick which mode:\n\n1:Show character first\n2:Show pinyin first\n3:Show either first\n4:Display meaning first\n5:Display all cards\n6:Quit\nTYPE 's' BEFORE THE NUMBER TO ONLY USE STARRED FLASHCARDS.\n\nChoice: ")
 print('\n')
 
+# Delete all non-special flashcards if the user activates special (starred) only mode.
+specials_only = False
+if choice[0] == 's':
+	specials_only = True
+	choice = choice[1:]
+	
+	# Put all special flashcards into temporary list, then use that to overwrite the FC list
+	FC_temp = []
+	for each_index in specials:
+		FC_temp = FC_temp + [FC[each_index]]
+	FC = FC_temp
+	
 # Perform program depending on choice
+choice = eval(choice)
 if choice < 5:
 	
 	# Create a randomized list of indices for the flashcards
 	numbers_list=[]
-	for count in range(len(lines)):
+	for count in range(len(FC)):
 		numbers_list=numbers_list+[count]
 	random.shuffle(numbers_list)
 	
 	# Show the character first
 	if choice == 1:
-		for count in range(len(lines)):
+		for count in range(len(FC)):
 			print(FC[numbers_list[count]][0])
 			pause()
 			print(FC[numbers_list[count]][1])
@@ -72,7 +89,7 @@ if choice < 5:
 
 	# Show the pinyin first
 	if choice == 2:
-		for count in range(len(lines)):
+		for count in range(len(FC)):
 			print(FC[numbers_list[count]][1])
 			pause()
 			print(FC[numbers_list[count]][0])
@@ -81,7 +98,7 @@ if choice < 5:
 
 	# Show either the character or pinyin first (chosen randomly each time)
 	if choice == 3:
-		for count in range(len(lines)):
+		for count in range(len(FC)):
 			category_shown = random.choice([0,1])
 			print(FC[numbers_list[count]][category_shown])
 			pause()
@@ -90,7 +107,7 @@ if choice < 5:
 			pause()	
 
 	if choice == 4:
-		for count in range(len(lines)):
+		for count in range(len(FC)):
 			print(FC[numbers_list[count]][2])
 			pause()
 			print(FC[numbers_list[count]][0])
@@ -99,7 +116,7 @@ if choice < 5:
 
 # Show all cards at once
 if choice == 5:
-	show_all(lines)
+	show_all()
 
 # Pause program until user presses a key (and print input for debug, if any)
 a=input("\nDone.")
